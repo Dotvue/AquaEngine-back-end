3f051d547504d35cf50f357687888769bc88f7b2
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 01:49:59 -0500

    feat(Profiles): add acl context facade.

---
+++ AquaEngine.API/Profiles/Application/ACL/ProfilesContextFacade.cs
@@ -0,0 +1,29 @@
+﻿using AquaEngine.API.Profiles.Domain.Model.Commands;
+using AquaEngine.API.Profiles.Domain.Model.Queries;
+using AquaEngine.API.Profiles.Domain.Services;
+using AquaEngine.API.Profiles.Interfaces.ACL;
+
+namespace AquaEngine.API.Profiles.Application.ACL;
+
+public class ProfilesContextFacade(IProfileCommandService profileCommandService, 
+    IProfileQueryService profileQueryService) : IProfilesContextFacade
+{
+    public async Task<int> CreateProfileAsync(string firstName, 
+        string lastName, string email, string phoneNumber, string dniNumber, int userId)
+    {
+        var createProfileCommand = new CreateProfileCommand(firstName, lastName, email, phoneNumber, dniNumber);
+
+        var profile = await profileCommandService.Handle(createProfileCommand, userId);
+        
+        return profile?.Id ?? 0;
+    }
+
+    public async Task<int> FetchProfileIdByUserId(int userId)
+    {
+        var getProfileByUserId = new GetProfileByUserIdQuery(userId);
+        
+        var profile = await profileQueryService.Handle(getProfileByUserId);
+        
+        return profile?.Id ?? 0;
+    }
+}
---
+++ AquaEngine.API/Profiles/Interfaces/ACL/IProfilesContextFacade.cs
@@ -0,0 +1,48 @@
+﻿namespace AquaEngine.API.Profiles.Interfaces.ACL;
+
+/// <summary>
+/// Profiles context facade interface: provides methods to interact with the profiles context.
+/// </summary>
+public interface IProfilesContextFacade
+{
+    /// <summary>
+    /// Creates a new profile.
+    /// </summary>
+    /// <param name="firstName">
+    /// First name.
+    /// </param>
+    /// <param name="lastName">
+    /// Last name.
+    /// </param>
+    /// <param name="email">
+    /// Email address.
+    /// </param>
+    /// <param name="phoneNumber">
+    /// Phone number.
+    /// </param>
+    /// <param name="dniNumber">
+    /// DNI number.
+    /// </param>
+    /// <returns>
+    /// The profile identifier.
+    /// </returns>
+    /// <param name="userId">
+    /// User identifier.
+    /// </param>
+    /// <returns>
+    /// The profile identifier.
+    /// </returns>
+    Task<int> CreateProfileAsync(string firstName, 
+        string lastName, string email, string phoneNumber, string dniNumber, int userId);
+    
+    /// <summary>
+    /// Fetches the profile identifier by user identifier.
+    /// </summary>
+    /// <param name="userId">
+    /// User identifier.
+    /// </param>
+    /// <returns>
+    /// The profile identifier.
+    /// </returns>
+    Task<int> FetchProfileIdByUserId(int userId);
+}
