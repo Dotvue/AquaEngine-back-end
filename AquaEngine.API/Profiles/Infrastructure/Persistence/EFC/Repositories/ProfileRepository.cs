b6f373ad9e1cad049e91f1b43aaa2aef96a25a27
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 00:52:13 -0500

    feat(Profiles): add profile repository implementation.

---
+++ AquaEngine.API/Profiles/Infrastructure/Persistence/EFC/Repositories/ProfileRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AquaEngine.API.Profiles.Domain.Repositories;
+using AquaEngine.API.Profiles.Domain.Model.Aggregates;
+using AquaEngine.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using AquaEngine.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace AquaEngine.API.Profiles.Infrastructure.Persistence.EFC.Repositories;
+
+public class ProfileRepository(AppDbContext context)
+    : BaseRepository<Profile>(context), IProfileRepository
+{
+    public async Task<Profile?> FindProfileByDni(string dni)
+    {
+        return await Context.Set<Profile>().FirstOrDefaultAsync(profile => profile.Dni.Number == dni);
+    }
+
+    public async Task<Profile?> FindProfileByPhoneNumber(string phoneNumber)
+    {
+        return await Context.Set<Profile>().FirstOrDefaultAsync(profile => profile.Phone.Number == phoneNumber);
+    }
+
+    public async Task<Profile?> FindProfileByUserId(int userId)
+    {
+        return await Context.Set<Profile>().FirstOrDefaultAsync(
+            profile => profile.UserIdentifier.Identifier == userId);
+    }
+}
