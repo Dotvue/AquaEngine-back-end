8d37f49d2d24d441734bf3b660c340fe0619650f
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 00:50:20 -0500

    refactor(Profiles): rename value object field "id number".

--- AquaEngine.API/Profiles/Domain/Model/ValueObjects/DniNumber.cs
+++ AquaEngine.API/Profiles/Domain/Model/ValueObjects/DniNumber.cs
@@ -4,15 +4,15 @@ namespace AquaEngine.API.Profiles.Domain.Model.ValueObjects;
 
 public record DniNumber()
 {
-    public string Dni { get; }
+    public string Number { get; }
 
-    public DniNumber(string dni = "00000000") : this()
+    public DniNumber(string number = "00000000") : this()
     {
-        if (string.IsNullOrEmpty(dni) || !Regex.IsMatch(dni, @"^\d{8}$"))
+        if (string.IsNullOrEmpty(number) || !Regex.IsMatch(number, @"^\d{8}$"))
         {
             throw new ArgumentException("DNI number must be 8 digits.");
         }
 
-        this.Dni = dni;
+        this.Number = number;
     }
 }
