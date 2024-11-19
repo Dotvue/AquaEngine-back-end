61670d85c201e9b94f0e199bd98c272279cc44b2
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 00:26:48 -0500

    feat(Profiles): add contracts to the profile repository.

---
+++ AquaEngine.API/Profiles/Domain/Repositories/IProfileRepository.cs
@@ -0,0 +1,42 @@
+﻿using AquaEngine.API.Profiles.Domain.Model.Aggregates;
+
+namespace AquaEngine.API.Profiles.Domain.Repositories;
+
+/// <summary>
+/// Profile repository: allows to find profiles by different criteria.
+/// </summary>
+public interface IProfileRepository
+{
+    /// <summary>
+    /// Finds a profile by its DNI.
+    /// </summary>
+    /// <param name="dni">
+    /// The DNI to search for.
+    /// </param>
+    /// <returns>
+    /// The profile with the given DNI, or null if no profile was found.
+    /// </returns>
+    Task<Profile?> FindProfileByDni(string dni);
+    
+    /// <summary>
+    /// Finds a profile by its phone number.
+    /// </summary>
+    /// <param name="phoneNumber">
+    /// The phone number to search for.
+    /// </param>
+    /// <returns>
+    /// The profile with the given phone number, or null if no profile was found.
+    /// </returns>
+    Task<Profile?> FindProfileByPhoneNumber(string phoneNumber);
+    
+    /// <summary>
+    /// Finds a profile by its user ID.
+    /// </summary>
+    /// <param name="userId">
+    /// The user ID to search for.
+    /// </param>
+    /// <returns>
+    /// The profile with the given user ID, or null if no profile was found.
+    /// </returns>
+    Task<Profile?> FindProfileByUserId(int userId);
+}
