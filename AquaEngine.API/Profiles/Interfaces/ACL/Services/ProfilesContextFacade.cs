930d3f595700a0c595fe80d7dffa869cf88a4936
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 10:40:09 -0500

    docs(Profiles): add missing documentation in profile context facade.

--- AquaEngine.API/Profiles/Interfaces/ACL/Services/ProfilesContextFacade.cs
+++ AquaEngine.API/Profiles/Interfaces/ACL/Services/ProfilesContextFacade.cs
@@ -1,23 +1,32 @@
 ﻿using AquaEngine.API.Profiles.Domain.Model.Commands;
 using AquaEngine.API.Profiles.Domain.Model.Queries;
 using AquaEngine.API.Profiles.Domain.Services;
-using AquaEngine.API.Profiles.Interfaces.ACL;
 
-namespace AquaEngine.API.Profiles.Application.ACL;
+namespace AquaEngine.API.Profiles.Interfaces.ACL.Services;
 
+/// <summary>
+/// Represents the profiles context facade.
+/// </summary>
+/// <param name="profileCommandService">
+/// The <see cref="IProfileCommandService"/> instance.
+/// </param>
+/// <param name="profileQueryService">
+/// The <see cref="IProfileQueryService"/> instance.
+/// </param>
 public class ProfilesContextFacade(IProfileCommandService profileCommandService, 
     IProfileQueryService profileQueryService) : IProfilesContextFacade
 {
-    public async Task<int> CreateProfileAsync(string firstName, 
-        string lastName, string email, string phoneNumber, string dniNumber, int userId)
+    // <inheritdoc/>
+    public async Task<int> CreateProfileAsync(string fullName, string email, string phoneNumber, string dniNumber, int userId)
     {
-        var createProfileCommand = new CreateProfileCommand(firstName, lastName, email, phoneNumber, dniNumber);
+        var createProfileCommand = new CreateProfileCommand(fullName, email, phoneNumber, dniNumber);
 
         var profile = await profileCommandService.Handle(createProfileCommand, userId);
         
         return profile?.Id ?? 0;
     }
 
+    // <inheritdoc/>
     public async Task<int> FetchProfileIdByUserId(int userId)
     {
         var getProfileByUserId = new GetProfileByUserIdQuery(userId);
