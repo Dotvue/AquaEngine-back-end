62beb43300b6458c38f258ae45159768e4569d8b
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 00:50:37 -0500

    feat(Profiles): add user id value object.

---
+++ AquaEngine.API/Profiles/Domain/Model/ValueObjects/UserId.cs
@@ -0,0 +1,16 @@
+﻿namespace AquaEngine.API.Profiles.Domain.Model.ValueObjects;
+
+public record UserId()
+{
+    public int Identifier { get; }
+
+    public UserId(int identifier=1) : this()
+    {
+        if (identifier <= 0)
+        {
+            throw new ArgumentException("User identifier must be greater than 0.");
+        }
+
+        Identifier = identifier;
+    }
+}
