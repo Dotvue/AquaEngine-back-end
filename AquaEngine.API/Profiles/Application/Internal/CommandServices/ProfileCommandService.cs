a679d302239ed227115a9057c546d8652b7d1545
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 01:21:51 -0500

    feat(Profiles): add profile command service implementation.

---
+++ AquaEngine.API/Profiles/Application/Internal/CommandServices/ProfileCommandService.cs
@@ -0,0 +1,42 @@
+﻿using AquaEngine.API.Profiles.Domain.Model.Aggregates;
+using AquaEngine.API.Profiles.Domain.Model.Commands;
+using AquaEngine.API.Profiles.Domain.Repositories;
+using AquaEngine.API.Profiles.Domain.Services;
+using AquaEngine.API.Shared.Domain.Repositories;
+
+namespace AquaEngine.API.Profiles.Application.Internal.CommandServices;
+
+/// <summary>
+/// Profile command service: manages the profile commands.
+/// </summary>
+/// <param name="profileRepository">
+/// Profile repository.
+/// </param>
+/// <param name="unitOfWork">
+/// Unit of work: manages the database transactions.
+/// </param>
+public class ProfileCommandService(IProfileRepository profileRepository, IUnitOfWork unitOfWork)
+    : IProfileCommandService
+{
+    public async Task<Profile?> Handle(CreateProfileCommand command, int userId)
+    {
+        if (profileRepository.ExistsByPhoneNumber(command.PhoneNumber))
+            throw new ArgumentException("Phone number already exists.");
+        
+        if (profileRepository.ExistsByDni(command.DniNumber))
+            throw new ArgumentException("DNI number already exists.");
+
+        var profile = new Profile(command, userId);
+
+        try
+        {
+            await profileRepository.AddAsync(profile);
+            await unitOfWork.CompleteAsync();
+            return profile;
+        }
+        catch (Exception e)
+        { 
+            return null;
+        }
+    }
+}
