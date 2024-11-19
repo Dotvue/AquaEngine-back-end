95c2b64f66df4c453557dd8a487be23a34c24ece
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 00:27:33 -0500

    feat(Profiles): add contracts to profile command and query services.

---
+++ AquaEngine.API/Profiles/Domain/Services/IProfileCommandService.cs
@@ -0,0 +1,21 @@
+﻿using AquaEngine.API.Profiles.Domain.Model.Aggregates;
+using AquaEngine.API.Profiles.Domain.Model.Commands;
+
+namespace AquaEngine.API.Profiles.Domain.Services;
+
+/// <summary>
+/// Profile command service: handles profile commands.
+/// </summary>
+public interface IProfileCommandService
+{
+    /// <summary>
+    /// Handles the create profile command.
+    /// </summary>
+    /// <param name="command">
+    /// Create profile command.
+    /// </param>
+    /// <returns>
+    /// The created profile.
+    /// </returns>
+    Task<Profile?> Handle(CreateProfileCommand command);
+}
---
+++ AquaEngine.API/Profiles/Domain/Services/IProfileQueryService.cs
@@ -0,0 +1,21 @@
+﻿using AquaEngine.API.Profiles.Domain.Model.Aggregates;
+using AquaEngine.API.Profiles.Domain.Model.Queries;
+
+namespace AquaEngine.API.Profiles.Domain.Services;
+
+/// <summary>
+/// Profile query service: handles profile queries.
+/// </summary>
+public interface IProfileQueryService
+{
+    /// <summary>
+    /// Handles the <see cref="GetProfileByUserIdQuery"/> query.
+    /// </summary>
+    /// <param name="query">
+    /// Query to handle.
+    /// </param>
+    /// <returns>
+    /// The profile with the specified user ID.
+    /// </returns>
+    Task<Profile?> Handle(GetProfileByUserIdQuery query);
+}
