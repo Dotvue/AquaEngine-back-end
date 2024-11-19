1dc4a97aeec67f696431e064eada02424c560b18
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-19 01:24:40 -0500

    feat(Profiles): add query service implementation.

---
+++ AquaEngine.API/Profiles/Application/Internal/QueryServices/ProfileQueryService.cs
@@ -0,0 +1,14 @@
+﻿using AquaEngine.API.Profiles.Domain.Model.Aggregates;
+using AquaEngine.API.Profiles.Domain.Model.Queries;
+using AquaEngine.API.Profiles.Domain.Repositories;
+using AquaEngine.API.Profiles.Domain.Services;
+
+namespace AquaEngine.API.Profiles.Application.Internal.QueryServices;
+
+public class ProfileQueryService(IProfileRepository profileRepository) : IProfileQueryService
+{
+    public Task<Profile?> Handle(GetProfileByUserIdQuery query)
+    {
+        return profileRepository.FindProfileByUserId(query.UserId);
+    }
+}
